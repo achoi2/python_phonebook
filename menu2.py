f0431c81878f77a399ef63eea69d877b5bece4ac
Andrew <andrewchoi1@hotmail.com>
2018-07-03 18:24:41 -0400

    changing phonebook to oop

--- menu2.py
+++ menu2.py
@@ -67,48 +67,59 @@ phonebook = {
     'sara': '333-3333'
 }
 
-def run_phonebook():
-    print menu
-    option = raw_input('What do you want to do? (1-5)')
-    if option in menu_options:
-            menu_option = menu_options[option]
-            print menu_option()
-    else:
-        print 'Invalid, try again'
-    return option != '5'
+class Phonebook(object):
 
-def find_contact():
-    name = raw_input('Enter Name:')
-    if name in phonebook:
-        return phonebook[name]
-    else:
-        return 'Not Valid' 
+    def __init__(self):
+        self.contacts = []
 
-def make_contact():
+# def run_phonebook():
+#     print menu
+#     option = raw_input('What do you want to do? (1-5)')
+#     if option in menu_options:
+#             menu_option = menu_options[option]
+#             print menu_option()
+#     else:
+#         print 'Invalid, try again'
+#     return option != '5'
+
+# def find_contact():
+#     name = raw_input('Enter Name:')
+#     if name in phonebook:
+#         return phonebook[name]
+#     else:
+#         return 'Not Valid' 
+
+# def make_contact():
+#     name = raw_input('Enter name:')
+#     phone = raw_input('Enter phone number:')
+#     phonebook[name] = phone
+#     return phonebook   
+
+def make_contact(self):
     name = raw_input('Enter name:')
     phone = raw_input('Enter phone number:')
-    phonebook[name] = phone
-    return phonebook   
 
-def delete_contact():
-    name = raw_input('Enter name:')
-    del phonebook[name]
-    return phonebook
 
-def list_contacts():
-    return phonebook
 
-def goodbye():
-    return 'Bye!'
+# def delete_contact():
+#     name = raw_input('Enter name:')
+#     del phonebook[name]
+#     return phonebook
 
-menu_options = {
-    '1': find_contact,
-    '2': make_contact,
-    '3': delete_contact,
-    '4': list_contacts,
-    '5': goodbye
-}
+# def list_contacts():
+#     return phonebook
 
-keep_running = True
-while keep_running:
-    keep_running = run_phonebook()
+# def goodbye():
+#     return 'Bye!'
+
+# menu_options = {
+#     '1': find_contact,
+#     '2': make_contact,
+#     '3': delete_contact,
+#     '4': list_contacts,
+#     '5': goodbye
+# }
+
+# keep_running = True
+# while keep_running:
+#     keep_running = run_phonebook()
